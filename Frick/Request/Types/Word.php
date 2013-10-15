4979e1516f6823ea2c0cb35e488d44fa50b60fc4
Bernhard Frick <gunta@alpenjodel.de>
2013-10-15 11:18:47 +0200

    New Data-Type: Word

---
+++ Frick/Request/Types/Word.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Frick\Request\Types;
+
+class Word extends AbstractType
+{
+    public function checkValue()
+    {
+        $wordRegex = "/^[a-zA-Z]+$/i";
+        $match = preg_match($wordRegex, $this->value);
+        if ($match === 1) {
+            $this->match = true;
+        } elseif ($match === 0) {
+            $this->match = false;
+        } else {
+            throw new \Exception("Syntax Error in Regular Expression.", 1);
+        }
+        return $this;
+    }
+    public function correctValue()
+    {
+        if (!$this->match && $this->doCorrection) {
+            $wordReplaceRegex = "/^[^a-zA-Z]+$/i";
+            $this->value = preg_replace($wordReplaceRegex, "", $this->value);
+        }
+        return $this;
+    }
+}
