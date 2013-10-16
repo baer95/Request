faa535146901db0b1a08bf271696846e21dbaf0a
Bernhard Frick <gunta@alpenjodel.de>
2013-10-16 23:26:27 +0200

    New Parser-Concept based on a Parser-Interface

---
+++ Frick/Request/Interfaces/ParserInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Frick\Request\Interfaces;
+
+interface ParserInterface
+{
+    public function __construct();
+
+    public function addData($key, $array);
+    public function getData($key = null);
+
+    public function setType($key, $type);
+    public function getType($key = null);
+
+    public function dataWalkRecursive($key, &$data, $types);
+
+    public function parse();
+}
+
