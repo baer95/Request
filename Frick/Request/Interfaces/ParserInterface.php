1e2c3119a75110d7111c34531d8082127a06e641
Bernhard Frick <gunta@alpenjodel.de>
2013-10-21 11:34:22 +0200

    Added Typecasting in Parser

--- Frick/Request/Interfaces/ParserInterface.php
+++ Frick/Request/Interfaces/ParserInterface.php
@@ -9,7 +9,7 @@ interface ParserInterface
     public function addData($key, $array);
     public function getData($key = null);
 
-    public function setType($key, $type);
+    public function setType($key, \Frick\Request\Interfaces\TypeInterface $type);
     public function getType($key = null);
 
     public function dataWalkRecursive($key, &$data, $types);
