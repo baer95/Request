e97745a548cff28b6310f6899ebf2bf15915236f
Bernhard Frick <gunta@alpenjodel.de>
2014-01-27 11:42:24 +0100

    TypeInterface is now deprecated
    TypeInterface is now deprecated and replaced by ValueInterface

--- lib/Request/Interfaces/TypeInterface.php
+++ lib/Request/Interfaces/TypeInterface.php
@@ -1,10 +1,13 @@
 <?php
 
+// DEPRECATED
+
 namespace Request\Interfaces;
 
 interface TypeInterface
 {
     public function __construct($value = null);
+
     public function setValue($value);
     public function getValue();
 
---
+++ lib/Request/Interfaces/ValueInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+// AKTUELL
+
+namespace Request\Interfaces;
+
+Interface ValueInterface
+{
+    public function __construct($value = false);
+
+    public function setInputValue($value);
+    public function getInputValue();
+
+    public function setDefaultValue($value);
+    public function getDefaultValue();
+
+    public function doMatch();
+    public function getMatch();
+
+    public function doCorrection();
+    public function getParsedValue();
+}
