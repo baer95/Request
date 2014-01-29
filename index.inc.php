78dd4d081070dfa07182e81f08a834ca9b318dc6
Bernhard Frick <gunta@alpenjodel.de>
2014-01-29 22:17:30 +0100

    Adapted tests to new Parser-system

--- index.inc.php
+++ index.inc.php
@@ -10,9 +10,8 @@
             <h3>Request</h3>
             <pre><?php
 
-
 $testing = array(
-    "boolean" => "false",
+    "boolean" => true,
     "email" => "bernard.frick@gmx.at",
     "email_dns" => "bernard.frick@gmx.at",
     "filename" => "testing.pdf",
@@ -20,8 +19,8 @@ $testing = array(
     "filesystemPath" => "D:/xampp/wwwroot/Request/index.php",
     "float" => 1.25,
     "integer" => 123,
-    "ipv4" => "85.124.157.100:80",
-    // "ipv6" => "2001:0db8:85a3:08d3:1319:8a2e:0370:7344",
+    "ipv4" => "85.124.157.100:21",
+    "ipv6" => "[2001::1319:8a2e:0370:85.124.157.100]:8080",
     "json" => '{"Herausgeber":"Xema","Nummer":"1234-5678-9012-3456","Deckung":2e+6,"Währung":"EURO","Inhaber":{"Name":"Mustermann","Vorname":"Max","männlich":true,"Hobbys":["Reiten","Golfen","Lesen"],"Alter":42,"Kinder":[],"Partner":null}}',
     "mimeType" => "application/pdf",
     "name" => "Bernhard Frick",
@@ -33,82 +32,58 @@ $testing = array(
     "word" => "wort",
 );
 
+$parser = new \Request\Parser\Parser($testing);
 
-$parser = new \Request\Parser\GPCParser();
-$parser->addData("testing", $testing);
+use Request\ValueObjects as Value;
 
-use Request\Types as Types;
+$parser->setType("boolean", new Value\Boolean());
+$parser->setDefaultValue("boolean", false);
 
-$parser->setType("boolean", new Types\Boolean());
-$parser->setType("email", new Types\Email());
-$parser->setType("email_dns", new Types\EmailDNS());
-$parser->setType("filename", new Types\Filename());
-$parser->setType("filesize", new Types\Filesize());
-$parser->setType("filesystemPath", new Types\FilesystemPath());
-$parser->setType("float", new Types\Float());
-$parser->setType("integer", new Types\Integer());
-$parser->setType("ipv4", new Types\IPv4());
-$parser->setType("ipv6", new Types\IPv6());
-$parser->setType("json", new Types\Json());
-$parser->setType("mimeType", new Types\MimeType());
-$parser->setType("name", new Types\Name());
-$parser->setType("numeric", new Types\Numeric());
-$parser->setType("password", new Types\Password());
-$parser->setType("string", new Types\String());
-$parser->setType("username", new Types\Username());
-$parser->setType("webpath", new Types\Webpath());
-$parser->setType("word", new Types\Word());
-$parser->setType("penis", new Types\Word());
+$parser->setType("email", new Value\Email());
+$parser->setDefaultValue("email", "");
+
+$parser->setType("float", new Value\Float());
+$parser->setDefaultValue("float", 1);
+
+$parser->setType("integer", new Value\Integer());
+$parser->setDefaultValue("integer", 1);
+
+$parser->setType("ipv4", new Value\IPv4());
+$parser->setDefaultValue("ipv4", "");
+
+$parser->setType("ipv6", new Value\IPv6());
+$parser->setDefaultValue("ipv6", "");
+
+$parser->setType("json", new Value\Json());
+$parser->setDefaultValue("json", "");
+
+$parser->setType("numeric", new Value\Numeric());
+$parser->setDefaultValue("numeric", "");
+
+// $parser->setType("email_dns", new Value\EmailDNS());
+// $parser->setType("filename", new Value\Filename());
+// $parser->setType("filesize", new Value\Filesize());
+// $parser->setType("filesystemPath", new Value\FilesystemPath());
+// $parser->setType("mimeType", new Value\MimeType());
+// $parser->setType("name", new Value\Name());
+// $parser->setType("password", new Value\Password());
+// $parser->setType("string", new Value\String());
+// $parser->setType("username", new Value\Username());
+// $parser->setType("webpath", new Value\Webpath());
+// $parser->setType("word", new Value\Word());
 
 try {
-    $parser->parse();
+    $parser->parseValues();
 } catch (Exception $e) {
     echo $e->getMessage();
     echo $e->getLine();
     echo $e->getFile();
 }
 
-$parsedTesting = $parser->getData("testing");
-print_r($parsedTesting);
-
-$parsed_GET = $parser->getData("_GET");
-print_r($parsed_GET);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Testing the FilesParser
-    // $filesParser = new \Request\Parser\FilesParser();
-    // print_r($filesParser->parse()->getData());
-
-
-
-// foreach ($testing as $key => $value) {
-//     echo $key.":\n";
-//     echo "\t".gettype($value)." ".print_r($value, true)."\n";
-//     echo "\t".gettype($parsed_testing[$key])." ".print_r($parsed_testing[$key], true)."\n\n";
-// }
-
-
-
-
+$matches = $parser->getMatchArray();
+print_r($matches);
+$values = $parser->getParsedValueArray();
+print_r($values);
 
             ?></pre>
         </div>
