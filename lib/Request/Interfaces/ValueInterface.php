d3e5e0d7c97c5d07d4b0f46ba574220afa046e84
Bernhard Frick <gunta@alpenjodel.de>
2014-02-23 00:40:51 +0100

    cleaning up with type and value interfaces

--- lib/Request/Interfaces/ValueInterface.php
+++ lib/Request/Interfaces/ValueInterface.php
@@ -4,17 +4,67 @@ namespace Request\Interfaces;
 
 Interface ValueInterface
 {
-    public function __construct($value = false);
+    /**
+     * Creates a new AbstractValue that checks and corrects your Request-Data. It sets the inputValue if given.
+     * @param mixed $inputValue The Value that should be checked and corrected.
+     */
+    public function __construct($inputValue = null);
 
-    public function setInputValue($value);
+    /**
+     * Sets the inputValue that sould be checked and corrected.
+     * @param mixed $inputValue The value that should be checked and corrected.
+     */
+    public function setInputValue($inputValue);
+
+    /**
+     * Gets the value that is proposed to be checked and corrected if there is no match.
+     * @return mixed The inputValue
+     */
     public function getInputValue();
 
-    public function setDefaultValue($value);
+    /**
+     * Sets the value to output if the check has failed.
+     * @param mixed $defaultValue The default Value.
+     */
+    public function setDefaultValue($defaultValue);
+
+    /**
+     * Gets the value that is proposed to be output if the check has failed.
+     * @return mixed The default Value
+     */
     public function getDefaultValue();
 
-    public function doMatch();
+    /**
+     * Resets the default value.
+     */
+    public function resetDefaultValue();
+
+    /**
+     * Gets the value thas been corrected if the inputValue-match failed.
+     * @return mixed The correted value
+     */
+    public function getCorrectedValue();
+
+    /**
+     * Gets the value that can be safely used in your code.
+     * @return mixed The outputValue
+     */
+    public function getOutputValue();
+
+    /**
+     * Gets wether the check has succeeded or not.
+     * @return bool The match or mismatch of inputValue.
+     */
     public function getMatch();
 
+    /**
+     * Check whether the inputValue matches the given Data-Type and do corrections if not matched.
+     */
     public function doCorrection();
-    public function getParsedValue();
+
+    /**
+     * Manages the correcting and matching-process
+     * @return bool True if the input matches the given data-type
+     */
+    public function execute();
 }
